d9cf598acbefdcdd93fb27dc35e0ada3f42ff2b2
AhmedAziz0047 <ahmedazizsouissi@gmail.com>
2021-11-08 00:53:58 +0100

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { VolsComponent } from './vols/vols.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VolsComponent
+    VolsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule

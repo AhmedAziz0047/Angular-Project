25fecc0865bec417628c8d1aba203f039cec5d86
AhmedAziz0047 <ahmedazizsouissi@gmail.com>
2021-11-07 18:31:05 +0100

    generate vols components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VolsComponent } from './vols/vols.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VolsComponent
   ],
   imports: [
     BrowserModule

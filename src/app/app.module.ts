5402b0df1741fdec3335dfcdc77920fd00dd2b10
AhmedAziz0047 <ahmedazizsouissi@gmail.com>
2021-11-08 01:26:18 +0100

    routing navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { VolsComponent } from './vols/vols.component';
 import { AppRoutingModule } from './app-routing.module';
 import { UserComponent } from './user/user.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VolsComponent,
-    UserComponent
+    UserComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,

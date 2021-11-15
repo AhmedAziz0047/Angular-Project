dd03399c8443cc5b9f8b9682ba6c4ec93ee7e93d
AhmedAziz0047 <ahmedazizsouissi@gmail.com>
2021-11-15 12:38:17 +0100

    routing newComponents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { VolsComponent } from './vols/vols.component';
 import { UserComponent } from './user/user.component';
 import { RouterModule,Routes } from '@angular/router';
 import { CarsComponent } from './cars/cars.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 
 const routes:Routes=[
   { path:'vols',component:VolsComponent },
   { path:'cars',component:CarsComponent },
+  { path:'login',component:LoginComponent},
+  { path:'signup',component:SignupComponent },
+  
   
 ]
 @NgModule({

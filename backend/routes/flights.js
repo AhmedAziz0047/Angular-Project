337ae893612b934299d8f98dbc7184fc0d3f0183
AhmedAziz0047 <ahmedazizsouissi@gmail.com>
2021-12-22 23:30:04 +0100

    creating RESERVATIONS/FLIGHTS routes

---
+++ backend/routes/flights.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const {getFlights}=require('./../Controller/Flights')
+
+
+router.get('/api/flights',getFlights);
+
+
+router.put('/api/res/id',(req,res)=>{
+
+});
+
+module.exports=router;

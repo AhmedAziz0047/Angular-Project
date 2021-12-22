99e9e2a9291dcbd0c2eb8d0a3d61d1291f2c218e
AhmedAziz0047 <ahmedazizsouissi@gmail.com>
2021-12-22 16:07:48 +0100

    adding services, fix CORS problem

--- src/app/services/flights.service.ts
+++ src/app/services/flights.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
 @Injectable({
   providedIn: 'root'
 })
 export class FlightsService {
-
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/api/flights';
+  constructor(private http:HttpClient) { }
 }

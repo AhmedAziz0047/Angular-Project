812104e1ee0b76425896c99d385198901277e3ba
AhmedAziz0047 <ahmedazizsouissi@gmail.com>
2021-12-24 01:29:58 +0100

    implementing Reservations crud

--- src/app/services/flights.service.ts
+++ src/app/services/flights.service.ts
@@ -1,9 +1,74 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from "@angular/common/http";
+// import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable()
+export class ConfigService {
+  constructor(private http: HttpClient) { }
+}
+
+
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class FlightsService {
-  private apiUrl = 'http://localhost:3000/api/flights';
-  constructor(private http:HttpClient) { }
+
+
+  private apiUrl = 'http://localhost:3000/api/';
+
+  constructor(private http: HttpClient) { }
+
+  all() {
+
+    return this.http.get(this.apiUrl + 'getFlights');
+
+  }
+
+
+  getbyid(id: any) {
+
+    return this.http.get(this.apiUrl + 'getFlightbyid/' + id);
+
+  }
+
+   delete(id: any){
+     return this.http.delete(this.apiUrl + 'deleteFlight/' + id);
+   }
+
+
+   update(id: any, data: any){
+     return this.http.put(this.apiUrl + 'editFlight/' + id , data);
+   }
+
+
+   add(data: any){
+     return this.http.post(this.apiUrl + 'addFlight' , data);
+   }
+
+
+
+
+
 }
+ // private apiUrl = 'http://localhost:3000/courses'; // json-server
+  // private apiUrl = 'https://my-json-server.typicode.com/houcem-h/public_courses_api/courses';
+// @Injectable({
+//   providedIn: 'root'
+// })
+
+
+
+// export class FlightsService {
+//   apiUrl = 'http://localhost:3000/api/getReservations';
+//   constructor(private http:HttpClient) { }
+//   getAll():Observable<any>{
+//     return this.http.get(this.apiUrl);
+//   }
+
+// }
---
+++ src/app/update-reservation-admin/update-reservation-admin.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { FlightsService } from '../services/flights.service';
+import { ReservationsService } from '../services/reservations.service';
+
+@Component({
+  selector: 'app-update-reservation-admin',
+  templateUrl: './update-reservation-admin.component.html',
+  styleUrls: ['./update-reservation-admin.component.css']
+})
+
+export class UpdateReservationAdminComponent implements OnInit {
+
+  constructor(private _reser: ReservationsService , private  route: ActivatedRoute) { }
+
+  id: any;
+  reservation = {
+
+    country: "",
+    nomclient: "",
+    prenomclient: 0,
+    email: 0,
+  };
+
+  rv: any;
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+
+    this._reser.getbyid(this.id).subscribe(
+      res=>{
+        this.rv= res;
+        this.reservation = this.rv
+      }
+    );
+
+
+  }
+
+  updateR(){
+
+    this._reser.update(this.id , this.reservation).subscribe(
+      res=>{
+
+      }
+    );
+    this.route.navigate(['/servers'],{relativeTo:this.route})
+
+  }
+
+}

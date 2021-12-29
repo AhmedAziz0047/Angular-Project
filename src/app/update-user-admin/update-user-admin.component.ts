ff0616fb3620875cac1140fc65e8602650b504cf
bilel713 <bilelbenaissa06@gmail.com>
2021-12-29 01:20:19 +0100

    update-user-admin/html/css

--- src/app/update-user-admin/update-user-admin.component.ts
+++ src/app/update-user-admin/update-user-admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { UsersService } from '../services/users.service';
 @Component({
   selector: 'app-update-user-admin',
   templateUrl: './update-user-admin.component.html',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateUserAdminComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private _user: UsersService , private  route: ActivatedRoute,private  router: Router) { }
+  id: any;
+  user = {
+    name: "",
+    email: "",
+    password: ""
+  };
+  fl: any;
   ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+
+    this._user.getbyid(this.id).subscribe(
+      res=>{
+        this.fl= res;
+        this.user = this.fl
+  });}
+ 
+  navigate(){
+    this.router.navigate(['/adminnav/adminusres']);
   }
 
+  update(){
+
+    this._user.update(this.id , this.user).subscribe(
+      res=>{
+
+      }
+    );
+
+  }
 }

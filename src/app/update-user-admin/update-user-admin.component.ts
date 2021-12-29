6f073b59d881f8ea17820965a7a751b192c7824b
bilel713 <bilelbenaissa06@gmail.com>
2021-12-29 01:08:49 +0100

    update-user-admin

---
+++ src/app/update-user-admin/update-user-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-user-admin',
+  templateUrl: './update-user-admin.component.html',
+  styleUrls: ['./update-user-admin.component.css']
+})
+export class UpdateUserAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

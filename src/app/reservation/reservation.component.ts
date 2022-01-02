e169e40ca5ab0502b0cd398f2261beef8159dd7f
AhmedAziz0047 <ahmedazizsouissi@gmail.com>
2022-01-02 20:42:17 +0100

    reservation validation

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -12,6 +12,7 @@ export class ReservationComponent implements OnInit {
 
   constructor(private toastr: ToastrService, private _reser: ReservationsService ,private flights: FlightsService , private  route: ActivatedRoute,private router:Router) { }
   id:any=this.route.snapshot.paramMap.get("id");
+  nom_ver = false;prenom_ver = false;email_ver = false;
   reservations={
     "country":"",
     "nomclient":"",
@@ -61,6 +62,16 @@ export class ReservationComponent implements OnInit {
   }
   addReservation()
     {
+      let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(this.reservations.prenomclient.length<3||this.reservations.prenomclient.length>15){
+        this.prenom_ver=true;this.toastr.warning("Warning","Verify your name");return;
+      }this.prenom_ver=false
+      if(this.reservations.nomclient.length<3||this.reservations.nomclient.length>15){
+        this.nom_ver=true;this.toastr.warning("Warning","Verify your last name");return;
+      }this.nom_ver=false
+      if(!re.test(this.reservations.email)){
+        this.email_ver=true;this.toastr.warning("Warning","Verify your email");return;
+      }this.email_ver=false
       this.reservations.country=this.flight.country;
       this.rv=this.reservations;
       this._reser.add(this.rv).subscribe(res=>{
@@ -68,7 +79,7 @@ export class ReservationComponent implements OnInit {
         this.toastr.success('You will receive your confimation number soon', 'Congatulation')
         this.router.navigate(['/vols'])
       },err=>{
-        this.toastr.warning("Warning","Verifyyour infos")      });
+        this.toastr.warning("Warning","Verify your infos")      });
       this.updatePlacesLeft();
 
 
